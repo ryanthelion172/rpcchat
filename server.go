e6f2a9ba7a181bdc857e7608f6e6e498213f87e5
@jake.jacobsen <jake.jacobsen@utahtech.edu>
2023-02-27 17:36:02 -0700

    tried to do server

--- server.go
+++ server.go
@@ -23,8 +23,20 @@ var shutdown chan struct{}
 
 func server(listenAddress string) {
         shutdown = make(chan struct{})
-        messages = make(map[string][]string)
-
+        messages = make(map[string][]string
+)
+	ln, err := net.Listen("tcp", listenAddress)
+	if err != nil {
+		log.Panicf("Can't bind port to listen. %q", err)
+	}
+	defer ln.close()
+	for {
+	    conn, err := ln.Accept()
+		if err != nil {
+		log.Panicln(err)
+	    }
+	go handleConnection(conn)
+	}
         // set up network listen and accept loop here
         // to receive and dispatch RPC requests
         // ...
